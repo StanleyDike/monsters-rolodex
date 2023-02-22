6f75fcdfaa562979616d5712e5a27f76b5a90ce8
Stanley Dike <stanley.dike21@gmail.com>
2023-02-22 18:50:21 -0500

    Class components, component state, setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      affiliation: 'Taylor Gang',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.affiliation}</p>
+          <button
+            onClick={() => {
+              this.setState({ affiliation: 'Glory Boys' });
+              // this.state.affiliation = 'Glory Boys';
+              // console.log(this.state);
+            }}
+          >
+            Change Affiliation
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

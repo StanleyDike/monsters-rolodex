d262645d6fd1f56855ae32f86e855879a9990823
Stanley Dike <stanley.dike21@gmail.com>
2023-02-25 15:38:26 -0500

    Input search box component, searching and filtering

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   // First time react renders a component onto the page
   async componentDidMount() {
+    console.log('componentDidMount');
     try {
       let response = await fetch(`https://jsonplaceholder.typicode.com/users`);
       let users = await response.json();
@@ -32,8 +34,24 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
